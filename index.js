4212b6f5f923754f24d4fce3af334a3ea0854ab1
alejandro armas <lxarmas@gmail.com>
2022-12-22 10:27:23 -0800

    Add files via upload
    first draft, still need to reference the databook

---
+++ index.js
@@ -0,0 +1,10 @@
+
+ import {bookData} from './book-data.js';
+ const myBookshelf = document.body.querySelector('.bookshelf');// dom element for Bookshelf
+
+ const myBook = document.body.querySelector('.book');// dom element for book
+
+
+    
+    
+
